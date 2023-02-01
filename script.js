ec49f555b313a60c31e65c45efda03f06cdbacb3
Cont0006361 <124114134+Cont0006361@users.noreply.github.com>
2023-02-01 03:28:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function updateMessage() {
-  $.getJSON("https://spreadsheets.google.com/feeds/cells/e/2PACX-1vSutQXMNVvXC5BEqHBgHFd2sVkPtDzFkEDpVOv4HIrLf2S8n5e0OrJ3AwggWalAthfxtDeuaHR-fiSD/1/public/basic?alt=json",
-  function(data) {
-    var entry = data.feed.entry[1]; // coluna A, linha 2
-    var message = entry.content.$t;
-    $("#message").text("Mensagem: " + message);
-  });
-}
 
-updateMessage();
-setInterval(updateMessage, 5000); // atualiza a cada 5 segundos
+const updateMessage = () => {
+  const sheetURL = "https://spreadsheets.google.com/feeds/cells/e/2PACX-1vSutQXMNVvXC5BEqHBgHFd2sVkPtDzFkEDpVOv4HIrLf2S8n5e0OrJ3AwggWalAthfxtDeuaHR-fiSD/1/public/basic?alt=json";
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", sheetURL, true);
+  xhr.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      const message = data.feed.entry[1].content.$t;
+      document.querySelector(".message").innerHTML = `Mensagem: ${message}`;
+
+      setTimeout(updateMessage, 5000);
+    }
+  };
+  xhr.send();
+};
