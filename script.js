ce1a1e7a6da7a36e65ed5b05756f4d199608db88
Cont0006361 <124114134+Cont0006361@users.noreply.github.com>
2023-02-01 03:35:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-
-const updateMessage = () => {
-  const sheetURL = "https://spreadsheets.google.com/feeds/cells/e/2PACX-1vSutQXMNVvXC5BEqHBgHFd2sVkPtDzFkEDpVOv4HIrLf2S8n5e0OrJ3AwggWalAthfxtDeuaHR-fiSD/1/public/basic?alt=json";
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", sheetURL, true);
-  xhr.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      const message = data.feed.entry[1].content.$t;
-      document.querySelector(".message").innerHTML = `Mensagem: ${message}`;
-
-      setTimeout(updateMessage, 5000);
-    }
-  };
-  xhr.send();
-};
+$(document).ready(function () {
+  setInterval(function () {
+    $.get("https://spreadsheets.google.com/feeds/cells/1_khL1gjUifjGXxJMs2hR0wBbW7zGmENlYUN8-w20tPE/od6/public/basic?alt=json", function (data) {
+      var entry = data.feed.entry;
+      var message = entry[1].content.$t;
+      $("#message").text("Mensagem: " + message);
+    });
+  }, 5000);
+});
